a546fad9bed01a05d9f040def27d258e4f8fdee7
Denis Singh <denis.darkrgd@gmail.com>
2021-05-27 19:13:44 +0300

    Added refs and notific

---
+++ src/js/notifications.js
@@ -0,0 +1,22 @@
+import { defaults, error, success } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+
+defaults.delay = 2000;
+defaults.maxTextHeight = null;
+
+function successRequest() {
+  success({
+    title: 'Success',
+    text: 'Successful implementation. Enjoy your viewing!',
+  });
+}
+
+function invalidRequest() {
+  error({
+    title: 'Oops',
+    text: 'Invalid request. Please enter a more correct request!',
+  });
+}
+
+export { successRequest, invalidRequest };
