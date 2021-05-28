d5213f831bc506295f38d55dfdfb3926f52a91ab
Denis Singh <denis.darkrgd@gmail.com>
2021-05-28 16:29:28 +0300

    Reduce delay

--- src/js/notifications.js
+++ src/js/notifications.js
@@ -2,7 +2,7 @@ import { defaults, error, success } from '@pnotify/core';
 import '@pnotify/core/dist/BrightTheme.css';
 import '@pnotify/core/dist/PNotify.css';
 
-defaults.delay = 2000;
+defaults.delay = 1000;
 defaults.maxTextHeight = null;
 
 function successRequest() {
